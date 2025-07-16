7d080bbd5b48a0d12f1a104b855d2c2fb0d6e20a
Dema <dimabarakat2000@gmail.com>
2025-07-16 18:13:43 +0300

    profile

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,9 @@ import 'package:caterease/features/authentication/presentation/controllers/bloc/
 import 'package:caterease/features/authentication/presentation/controllers/bloc/verify/verify_bloc.dart';
 import 'package:caterease/features/authentication/presentation/screens/login_screen.dart';
 import 'package:caterease/features/authentication/presentation/screens/register_screen.dart';
+import 'package:caterease/features/delivery/presentation/screens/my_order.dart';
+import 'package:caterease/features/profile/presentation/screens/profile_page.dart';
+import 'package:caterease/features/profile/presentation/screens/setting_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:caterease/features/restaurants/presentation/bloc/restaurants_bloc.dart';
@@ -37,7 +40,7 @@ class MyApp extends StatelessWidget {
         title: 'Caterease',
         theme: AppTheme.lightTheme,
         navigatorObservers: [routeObserver],
-        home: const LoginPage(),
+        home: MyOrder(),
         // home:  AnimatedSplashScreen(),
       ),
     );
